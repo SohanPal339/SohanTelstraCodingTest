5f86e60cca42cc27c20fddbbe75c81f82c0a3c2f
Sohan Pal <sohan.sharma339@gmail.com>
2019-11-14 16:36:58 +0530

    Bug fixed

---
+++ app/src/main/java/telstra/sohan/com/sohantelstracodingtest/utility/GlideDownloadImage.kt
@@ -0,0 +1,23 @@
+package telstra.sohan.com.sohantelstracodingtest.utility
+
+import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import telstra.sohan.com.sohantelstracodingtest.R
+
+
+object GlideDownloadImage {
+
+    fun downloadImage(c: Context, url: String?, img: ImageView) {
+
+        Glide.with(c)
+                .load(url) // Image URL
+                .centerCrop()
+                .crossFade()
+                .override(100, 60)
+                .placeholder(R.drawable.place_holder) // Place holder image
+                .error(R.drawable.place_holder) // On error image
+                .into(img)
+
+    }
+}
